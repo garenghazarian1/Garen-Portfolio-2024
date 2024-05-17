c9ad4ae2519c69067ed75800106b7a1d7f5ed375
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-05-17 14:38:59 +0200

    fix mysite styling

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,55 +1,12 @@
-"use client";
-import styles from "./Contact.module.css";
-import Tilt from 'react-parallax-tilt';
+import Contact from '@/components/contact/Contact'
+import styles from "./ContactMe.module.css"
+import Link from 'next/link'
 
-const Contact = () => {
-  
-  const store = {
-    mobile: '+49 (0)15257398979',
-    email: 'garenghazarian1@gmail.com',
-    street: 'Ottilie-Hoffmann-Straße 40',
-    city: 'Bremen',
-    state: 'Bremen', 
-    zipCode: '28213'
-  };
-
+export default function page() {
   return (
-    <div className={styles.container}>
-      <div className={styles.flex}>
-        
-        <Tilt
-          className={styles.imageWrapper}
-          tiltMaxAngleX={10}
-          tiltMaxAngleY={10}
-          glareEnable={true}
-          glareMaxOpacity={0.5}
-          scale={1.05}
-        >
-          <div className={styles.section}>
-            <h2 className={styles.sectionHeader}>Contact Me</h2>
-            <div>
-              <p className={styles.p}>{store.street}</p>
-              <p className={styles.p}>{store.city}, {store.zipCode}</p>
-              <p className={styles.p}>Germany</p>
-            </div>
-            <div style={{ marginTop: '32px' }}>
-              <p className={styles.p}>Mobile: <a className={styles.a} href={`tel:${store.mobile}`}>{store.mobile}</a></p>
-              <p className={styles.p}>Email: <a className={styles.a} href={`mailto:${store.email}`}>{store.email}</a></p>
-            </div>
-            <div className={styles.mapContainer}>
-              <iframe
-                className={styles.map}
-                src={`https://www.google.com/maps?q=${encodeURIComponent(store.street + ', ' + store.city + ', ' + store.zipCode)}&output=embed`}
-                allowFullScreen=""
-                loading="lazy"
-                title="Store Location"
-              ></iframe>
-            </div>
-          </div>
-        </Tilt>
-      </div>
-    </div>
-  );
-};
-
-export default Contact;
+    <>
+    <Link href="/" className={styles.nav} >Back to Home</Link>
+    <Contact/>
+    </>
+  )
+}
