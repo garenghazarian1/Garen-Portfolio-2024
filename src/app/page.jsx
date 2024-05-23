0ad4dfa430ff5cd286954fac11b8d23715824a06
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-05-23 18:26:05 +0200

    change icon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 
 import MainPage from "@/components/mainPage/MainPage";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
     <MainPage/>
     </>
   );
