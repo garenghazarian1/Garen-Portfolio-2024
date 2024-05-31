efc92b17d8e515d64744aacbb0bb956fea4ed34e
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-05-31 20:21:07 +0200

    more styling main page

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -6,36 +6,49 @@ import Link from 'next/link';
 
 import {abril, sevillana, bad, pt} from "@/app/ui/fonts"
 
-
-// className={`${abril.className} `}
 export default function MainPage() {
     const [isLoading, setIsLoading] = useState(true);
+    const [fontClass, setFontClass] = useState(bad.className);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    },500);
 
-    return () => clearTimeout(timer);
-  }, []);
+    // Interval to change fonts every 2 minutes
+    const fontInterval = setInterval(() => {
+      setFontClass(currentFont => {
+          // Cycle through fonts
+          if (currentFont === bad.className) return abril.className;
+          if (currentFont === abril.className) return sevillana.className;
+          if (currentFont === sevillana.className) return pt.className;
+          return bad.className;
+      });
+  }, 2000); // 120000 milliseconds = 2 minutes
+
+  return () => {
+    clearTimeout(timer);
+    clearInterval(fontInterval); // Clear interval on component unmount
+};
+}, []);
 
   if (isLoading) {
     return <Loading />;
   }
   return (
     <>
-      <main >
-        <div className={styles.container}>
-          <div className={styles.container1} >
-            <Link href="/me" className={`${bad.className} ${styles.child1}`}>ME</Link>
-            <Link href="/projects" className={`${bad.className}  ${styles.child2}`}>PROJECTS</Link>
-          </div>
-          <div className={styles.container1} >
-            <Link href="/contact" className={`${bad.className}  ${styles.child3}`}>CONTACT</Link>
-            <Link href="/qualifications" className={`${bad.className}  ${styles.child4}`}>QUALIFICATIONS</Link>
-          </div>
-        </div>
-      </main>
+        <main>
+            <div className={styles.container}>
+                <div className={styles.container1}>
+                    <Link href="/me" className={`${fontClass} ${styles.child1}`}>ME</Link>
+                    <Link href="/projects" className={`${bad.className} ${styles.child2}`}>PROJECTS</Link>
+                </div>
+                <div className={styles.container1}>
+                    <Link href="/contact" className={`${bad.className} ${styles.child3}`}>CONTACT</Link>
+                    <Link href="/qualifications" className={`${bad.className} ${styles.child4}`}>QUALIFICATIONS</Link>
+                </div>
+            </div>
+        </main>
     </>
-  )
+);
 }
