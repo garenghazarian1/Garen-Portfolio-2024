5e8fdef5a353d4a836de6dbb099b5b94c1300f9a
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-07-24 15:32:53 +0200

    test12

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,10 @@
 "use client";
 import NotFounded from "@/components/notFound/NotFounded";
-import { useTranslations } from "next-intl";
-import Link from "next/link";
 
 export default function NotFound() {
-  const t = useTranslations("Me");
   return (
     <>
       <NotFounded />
-      <Link href="/" className="nav">
-        {t("backToHome")}
-      </Link>
     </>
   );
 }
--- src/components/notFound/NotFounded.jsx
+++ src/components/notFound/NotFounded.jsx
@@ -1,15 +1,19 @@
-"use client";
+import Link from "next/link";
 import styles from "./NotFound.module.css";
-import { useTranslations } from "next-intl";
 
 export default function NotFounded() {
-  const t = useTranslations("NotFound");
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.header}>{t("header")}</h1>
-        <p className={styles.subHeader}>{t("subHeader")}</p>
-        <p className={styles.description}>{t("description")}</p>
+        <h1 className={styles.header}>404</h1>
+        <p className={styles.subHeader}>Oops! Page not found.</p>
+        <p className={styles.description}>
+          The page you are looking for does not exist or has been moved.
+        </p>
+
+        <Link href="/" className={styles.linkButton}>
+          Back to Home
+        </Link>
       </div>
     </div>
   );
