795eafd10368b32b5d797eb7ca3c1bcc52c8566e
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-07-25 23:00:23 +0200

    style main page

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -38,74 +38,60 @@ export default function MainPage() {
   }
   return (
     <>
-      <main>
-        <div className={styles.container}>
-          <div className={styles.container1}>
-            <div className={` ${styles.child1}`}>
-              <Link href="/me" className={`${fontClass} ${styles.child1}`}>
-                <div className={styles.meMobile}>{t("me")}</div>
+      <div className={styles.container}>
+        <div className={styles.container1}>
+          <div className={` ${styles.child1}`}>
+            <div className={` ${styles.child1A}`}>
+              <Link href="/me" className={`${fontClass} `}>
+                {t("me")}
               </Link>
-
-              <div className={styles.childDiv}>
-                <Link href="/me/mySlides" className={` ${styles.child}`}>
-                  {t("slide")}
-                </Link>
-                <Link href="/me/myPhoto" className={` ${styles.child}`}>
-                  {t("photos")}
-                </Link>
-                <Link href="/me/about" className={` ${styles.child}`}>
-                  {t("aboutMe")}
-                </Link>
-                <Link href="/me/follow" className={` ${styles.child}`}>
-                  {t("followMe")}
-                </Link>
-              </div>
             </div>
 
-            {/* ${styles.child2} */}
-            <div className={styles.child2}>
-              <div className={styles.childProject}>
-                <Link
-                  href="/projects"
-                  className={`${bad.className} ${styles.childProjectButton} `}
-                >
-                  {t("projects")}
-                </Link>
-              </div>
-
-              <div className={styles.childProject}>
-                <div
-                  className={`${bad.className} ${styles.childProjectHeader}   `}
-                >
-                  <h2
-                    className={`${bad.className} ${styles.childProjectHeader}   `}
-                  >
-                    {t("services")}
-                  </h2>
-                </div>
-                <div className={`${styles.childProjectButton}`}>
-                  <Link href="/projects/qrCode">{t("qr")}</Link>
-                </div>
-              </div>
+            <div className={styles.child1B}>
+              <Link href="/me/mySlides" className={` ${styles.child1C}`}>
+                {t("slide")}
+              </Link>
+              <Link href="/me/myPhoto" className={` ${styles.child1C}`}>
+                {t("photos")}
+              </Link>
+              <Link href="/me/about" className={` ${styles.child1C}`}>
+                {t("aboutMe")}
+              </Link>
+              <Link href="/me/follow" className={` ${styles.child1C}`}>
+                {t("followMe")}
+              </Link>
             </div>
           </div>
 
-          <div className={styles.container1}>
-            <Link
-              href="/contact"
-              className={`${bad.className} ${styles.child3}`}
-            >
-              {t("contact")}
-            </Link>
-            <Link
-              href="/qualifications"
-              className={`${bad.className} ${styles.child4}`}
-            >
-              {t("qualifications")}
-            </Link>
+          {/* ${styles.child2} */}
+          <div className={styles.child2}>
+            <div className={`${bad.className} ${styles.child2B} `}>
+              <Link href="/projects">{t("projects")}</Link>
+            </div>
+
+            <div className={styles.child2B}>
+              <div className={`${bad.className} ${styles.child2C} `}>
+                <h2>{t("services")}</h2>
+              </div>
+              <div className={`${styles.child2C} `}>
+                <Link href="/projects/qrCode">{t("qr")}</Link>
+              </div>
+            </div>
           </div>
         </div>
-      </main>
+
+        <div className={styles.container1}>
+          <Link href="/contact" className={`${bad.className} ${styles.child3}`}>
+            {t("contact")}
+          </Link>
+          <Link
+            href="/qualifications"
+            className={`${bad.className} ${styles.child4}`}
+          >
+            {t("qualifications")}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
