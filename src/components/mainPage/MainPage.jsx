61946f6c0b5466d3dcdbd784b7cb2c2c667592de
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-12-30 23:08:26 +0100

    change the main page style new desigen

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -5,83 +5,100 @@ import styles from "./Main.module.css";
 // import Link from 'next/link';
 import { Link } from "@/navigation";
 import { useTranslations } from "next-intl";
-import { abril, sevillana, bad, pt } from "@/app/ui/fonts";
+import { bad } from "@/app/ui/fonts";
+// import { abril, sevillana, bad, pt } from "@/app/ui/fonts";
 
 export default function MainPage() {
   const [isLoading, setIsLoading] = useState(true);
-  const [fontClass, setFontClass] = useState(bad.className);
+  // // const [fontClass, setFontClass] = useState(bad.className);
   const t = useTranslations("Index");
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 500);
 
-    // Interval to change fonts every 2 minutes
-    const fontInterval = setInterval(() => {
-      setFontClass((currentFont) => {
-        // Cycle through fonts
-        if (currentFont === bad.className) return abril.className;
-        if (currentFont === abril.className) return sevillana.className;
-        if (currentFont === sevillana.className) return pt.className;
-        return bad.className;
-      });
-    }, 2000);
-
-    return () => {
-      clearTimeout(timer);
-      clearInterval(fontInterval);
-    };
+    return () => clearTimeout(timer); // Cleanup timeout
   }, []);
 
   if (isLoading) {
     return <Loading />;
   }
+
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.container1}>
-          <div className={` ${styles.child1}`}>
-            <Link href="/me" className={`${fontClass} ${styles.child1A} `}>
-              {t("me")}
-            </Link>
+      <div className={`${styles.container} `}>
+        <div className={`${styles.containerBackground} ${styles.glow}  `}>
+          <div className={` ${styles.el}`}></div>
 
-            <div className={styles.child1B}>
-              <Link href="/me/mySlides" className={` ${styles.child1C}`}>
-                {t("slide")}
-              </Link>
-              <Link href="/me/myPhoto" className={` ${styles.child1C}`}>
-                {t("photos")}
-              </Link>
-              <Link href="/me/about" className={` ${styles.child1C}`}>
-                {t("aboutMe")}
-              </Link>
-              <Link href="/me/follow" className={` ${styles.child1C}`}>
-                {t("followMe")}
-              </Link>
+          <div className={`${styles.container1}  `}>
+            <div className={` ${styles.child1} `}>
+              <div className={`${styles.child1A} `}>
+                <Link
+                  href="/me"
+                  className={` ${bad.className} ${styles.child1ALink} `}
+                >
+                  {t("me")}
+                </Link>
+                {/* <div className={` ${styles.elA}`}></div> */}
+              </div>
+
+              <div className={styles.child1B}>
+                <Link
+                  href="/me/mySlides"
+                  className={`${bad.className} ${styles.child1BLink} `}
+                >
+                  {t("slide")}
+                </Link>
+                <Link
+                  href="/me/myPhoto"
+                  className={`${bad.className} ${styles.child1BLink}`}
+                >
+                  {t("photos")}
+                </Link>
+                <Link
+                  href="/me/about"
+                  className={`${bad.className} ${styles.child1BLink}`}
+                >
+                  {t("aboutMe")}
+                </Link>
+                <Link
+                  href="/me/follow"
+                  className={`${bad.className} ${styles.child1BLink}`}
+                >
+                  {t("followMe")}
+                </Link>
+              </div>
             </div>
           </div>
 
           {/* ${styles.child2} */}
-          <div className={styles.child2}>
-            <div className={`${bad.className} ${styles.child2B} `}>
-              <Link href="/projects">{t("projects")}</Link>
-            </div>
+          <div className={`${styles.container2}  `}>
+            <div className={styles.child2}>
+              <Link
+                href="/projects"
+                className={`${styles.child2ALink} ${bad.className} ${styles.child2A} `}
+              >
+                {t("projects")}
+              </Link>
 
-            <div className={styles.child2B}>
-              {/* <div className={` ${styles.child2CText} `}>
-                <h2 className={`${bad.className}`}>{t("services")}</h2>
-              </div> */}
-              <div className={`${styles.child2C} `}>
-                <Link href="/projects/qrCode">{t("qr")}</Link>
-              </div>
-              <div className={`${styles.child2C} `}>
-                <Link href="/projects/bmi">BMI</Link>
-              </div>
+              <Link
+                href="/projects/qrCode"
+                className={`${styles.child2BLink} ${styles.child2B} ${bad.className} `}
+              >
+                {t("qr")}
+              </Link>
+
+              <Link
+                href="/projects/bmi"
+                className={`${styles.child2CLink} ${styles.child2C} ${bad.className}`}
+              >
+                BMI
+              </Link>
             </div>
           </div>
         </div>
-
-        <div className={styles.container1}>
+        {/* container3 */}
+        <div className={styles.container3}>
           <Link href="/contact" className={`${bad.className} ${styles.child3}`}>
             {t("contact")}
           </Link>
--- src/hooks/useTranslation.jsx
+++ src/hooks/useTranslation.jsx
@@ -1,8 +1,8 @@
 "use client";
-import { usePathname } from 'next/navigation';
-import { useState, useEffect } from 'react';
-import enTranslations from '../../public/locales/en/common.json';
-import deTranslations from '../../public/locales/de/common.json';
+import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import enTranslations from "../../public/locales/en/common.json";
+import deTranslations from "../../public/locales/de/common.json";
 
 const translations = {
   en: enTranslations,
@@ -11,20 +11,22 @@ const translations = {
 
 export const useTranslation = () => {
   const pathname = usePathname();
-  const [locale, setLocale] = useState('en');
+  const [locale, setLocale] = useState("en");
 
   useEffect(() => {
     // Extract the locale from the pathname, e.g., /en/page or /de/page
-    const currentLocale = pathname.split('/')[1];
+    const currentLocale = pathname.split("/")[1];
     if (translations[currentLocale]) {
       setLocale(currentLocale);
     }
   }, [pathname]);
 
-  const [translation, setTranslation] = useState(translations[locale] || translations['en']);
+  const [translation, setTranslation] = useState(
+    translations[locale] || translations["en"]
+  );
 
   useEffect(() => {
-    setTranslation(translations[locale] || translations['en']);
+    setTranslation(translations[locale] || translations["en"]);
   }, [locale]);
 
   const t = (key) => translation[key] || key;
