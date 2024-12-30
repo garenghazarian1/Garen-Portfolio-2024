24dd21a60f5e3898179afd14c8c1a816bceb1c8f
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-12-31 00:14:42 +0100

    edit slide

--- src/components/me/mySlide/MySlide.jsx
+++ src/components/me/mySlide/MySlide.jsx
@@ -1,162 +1,97 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import styles from "./MySlide.module.css";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import Image from "next/image";
-
 import { useTranslations } from "next-intl";
 
 export default function MySlide() {
+  const [showIntro, setShowIntro] = useState(true);
+
   const t = useTranslations("Slides");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 3000); // 3 seconds for the intro
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <div className={styles.container}>
-        <Swiper
-          pagination={{ type: "progressbar" }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className={styles.swiper}
-        >
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/001.png"
-                width={500}
-                height={500}
-                alt="001.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("introduction.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>
-                {t("introduction.text")}
-              </p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/002.png"
-                width={500}
-                height={500}
-                alt="002.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("earlyCareer.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>{t("earlyCareer.text")}</p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/003.png"
-                width={500}
-                height={500}
-                alt="003.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("movingToBelgium.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>
-                {t("movingToBelgium.text")}
-              </p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/004.png"
-                width={500}
-                height={500}
-                alt="004.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("personalLife.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>
-                {t("personalLife.text")}
-              </p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/005.png"
-                width={500}
-                height={500}
-                alt="005.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("techInGermany.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>
-                {t("techInGermany.text")}
-              </p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/006.png"
-                width={500}
-                height={500}
-                alt="006.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("currentProject.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>
-                {t("currentProject.text")}
-              </p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideImg}>
-              <Image
-                src="/007.png"
-                width={500}
-                height={500}
-                alt="007.png"
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.swiperSlideText}>
-              <h2 className={styles.swiperSlideTextH}>
-                {t("conclusion.title")}
-              </h2>
-              <p className={styles.swiperSlideTextP}>{t("conclusion.text")}</p>
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </>
+    <div className={styles.wrapper}>
+      {showIntro ? (
+        <div className={styles.intro}>
+          <h1 className={styles.introText}>Welcome to My Journey</h1>
+        </div>
+      ) : (
+        <div className={styles.container}>
+          <Swiper
+            pagination={{ clickable: true }}
+            navigation={true}
+            autoplay={{ delay: 5000, disableOnInteraction: false }}
+            modules={[Pagination, Navigation, Autoplay]}
+            className={styles.swiper}
+          >
+            {[
+              {
+                src: "/001.png",
+                title: t("introduction.title"),
+                text: t("introduction.text"),
+              },
+              {
+                src: "/002.png",
+                title: t("earlyCareer.title"),
+                text: t("earlyCareer.text"),
+              },
+              {
+                src: "/003.png",
+                title: t("movingToBelgium.title"),
+                text: t("movingToBelgium.text"),
+              },
+              {
+                src: "/004.png",
+                title: t("personalLife.title"),
+                text: t("personalLife.text"),
+              },
+              {
+                src: "/005.png",
+                title: t("techInGermany.title"),
+                text: t("techInGermany.text"),
+              },
+              {
+                src: "/006.png",
+                title: t("currentProject.title"),
+                text: t("currentProject.text"),
+              },
+              {
+                src: "/007.png",
+                title: t("conclusion.title"),
+                text: t("conclusion.text"),
+              },
+            ].map((slide, index) => (
+              <SwiperSlide key={index} className={styles.swiperSlide}>
+                <div className={styles.imageWrapper}>
+                  <Image
+                    src={slide.src}
+                    width={500}
+                    height={500}
+                    alt={`Slide ${index + 1}`}
+                    className={styles.img}
+                  />
+                </div>
+                <div className={styles.textWrapper}>
+                  <h2 className={styles.title}>{slide.title}</h2>
+                  <p className={styles.text}>{slide.text}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </div>
   );
 }
