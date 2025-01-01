50e7ec6363dc58eecbe44a4e29fe6f0d512988c1
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2025-01-01 22:52:28 +0100

    restyle photo part

--- src/components/me/myPhotos/MyPhotos.jsx
+++ src/components/me/myPhotos/MyPhotos.jsx
@@ -8,13 +8,23 @@ import { EffectFlip, Pagination } from "swiper/modules";
 import Image from "next/image";
 import styles from "./MyPhotos.module.css";
 
-const images = ["/g01a.jpg", "/g02.jpg", "/g03.jpg"];
+const images = [
+  { src: "/g01a.jpg", alt: "Me in EU", description: "Exploring Europe" },
+  { src: "/g02.jpg", alt: "Me in Nature", description: "Serenity in Nature" },
+  {
+    src: "/g03.jpg",
+    alt: "Me at Grand Place",
+    description: "Grand Place Memories",
+  },
+];
 
 export default function MyPhotos() {
-  const [backgroundImage, setBackgroundImage] = useState(images[0]);
+  const [backgroundImage, setBackgroundImage] = useState(images[0].src);
+  const [caption, setCaption] = useState(images[0].description);
 
   const handleSlideChange = (swiper) => {
-    setBackgroundImage(images[swiper.activeIndex]);
+    setBackgroundImage(images[swiper.activeIndex].src);
+    setCaption(images[swiper.activeIndex].description);
   };
 
   return (
@@ -22,47 +32,33 @@ export default function MyPhotos() {
       className={styles.imageSlider}
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
+      <div className={styles.overlay}></div>
+      <div className={styles.caption}>
+        <h2>{caption}</h2>
+      </div>
       <Swiper
         effect="flip"
         grabCursor={true}
-        flipEffect={{
-          slideShadows: true,
-        }}
+        flipEffect={{ slideShadows: true }}
         pagination={{ clickable: true }}
         modules={[EffectFlip, Pagination]}
         className={styles.swiper}
         onSlideChange={handleSlideChange}
       >
-        <SwiperSlide className={styles.swiperSlide}>
-          <Image
-            src="/g01a.jpg"
-            alt="Me in EU"
-            width={500}
-            height={500}
-            priority={false} // {false} | {true}
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>
-          <Image
-            src="/g02.jpg"
-            alt="Me in Nature"
-            width={500}
-            height={500}
-            priority={false} // {false} | {true}
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>
-          <Image
-            src="/g03.jpg"
-            alt="Me at Grand Place"
-            width={500}
-            height={500}
-            priority={false} // {false} | {true}
-            className={styles.img}
-          />
-        </SwiperSlide>
+        {images.map((image, index) => (
+          <SwiperSlide key={index} className={styles.swiperSlide}>
+            <div className={styles.slideContent}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={600}
+                height={600}
+                className={styles.img}
+                priority
+              />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
