f631d62a8abcc70184c31b0e9f985a788b5d7d30
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2025-01-02 08:18:59 +0100

    edit footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,49 +1,44 @@
-import Image from "next/image";
 import styles from "./Footer.module.css";
 import { useTranslations } from "next-intl";
 
 export default function Footer() {
   const t = useTranslations("Footer");
   return (
-    <>
-      <div className={styles.socialLinks}>
-        <a
-          className={styles.infoA}
-          href="/Garen-Ghazarian-eng-11-sep-2024.pdf"
-          target="_blank"
-        >
-          <p>{t("downloadResume")}</p>
-
-          <div>
-            <p className={styles.imageContainer}>{t("pdfLabel")}</p>
-          </div>
-        </a>
-        <a
-          className={styles.infoA}
-          href="https://www.linkedin.com/in/garen-ghazarian-1692812a5/"
-          target="_blank"
-        >
-          {t("linkedin")}
-        </a>
-        <a
-          className={styles.infoA}
-          href="https://github.com/garenghazarian1"
-          target="_blank"
-        >
-          {t("github")}
-        </a>
-        <a
-          className={styles.infoA}
-          href="/Garen-Ghazarian-full-11-sep-2024.pdf"
-          target="_blank"
-        >
-          <p>{t("downloadFullResume")}</p>
-
-          <div>
-            <p className={styles.imageContainer}>{t("pdfLabel")}</p>
-          </div>
-        </a>
-      </div>
-    </>
+    <div className={styles.socialLinks}>
+      <a
+        className={styles.infoA}
+        href="/Garen-Ghazarian-eng-11-sep-2024.pdf"
+        target="_blank"
+      >
+        <p>{t("downloadResume")}</p>
+        <div>
+          <p className={styles.imageContainer}>{t("pdfLabel")}</p>
+        </div>
+      </a>
+      <a
+        className={styles.infoA}
+        href="/Garen-Ghazarian-full-11-sep-2024.pdf"
+        target="_blank"
+      >
+        <p>{t("downloadFullResume")}</p>
+        <div>
+          <p className={styles.imageContainer}>{t("pdfLabel")}</p>
+        </div>
+      </a>
+      <a
+        className={styles.infoA}
+        href="https://www.linkedin.com/in/garen-ghazarian-1692812a5/"
+        target="_blank"
+      >
+        {t("linkedin")}
+      </a>
+      <a
+        className={styles.infoA}
+        href="https://github.com/garenghazarian1"
+        target="_blank"
+      >
+        {t("github")}
+      </a>
+    </div>
   );
 }
